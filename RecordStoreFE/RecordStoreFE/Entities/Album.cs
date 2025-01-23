bc368acbf477604282d07d7ef89560b9afa2190c
Christopher Lee <leecgh@gmail.com>
2025-01-23 13:21:05 +0000

    Implement basic homepage layout

---
+++ RecordStoreFE/RecordStoreFE/Entities/Album.cs
@@ -0,0 +1,16 @@
+﻿namespace RecordStoreFE.Classes;
+
+public class Album
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public string Artist { get; set; } = "";
+
+    public string Composer { get; set; } = "";
+
+    public Genre Genre { get; set; }
+
+    public int Year { get; set; }
+}
---
+++ RecordStoreFE/RecordStoreFE/Entities/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordStoreFE.Classes;
+
+public enum Genre
+{
+    Classical = 1,
+    Opera = 2,
+    Jazz = 3,
+    Rock = 4,
+    Pop = 5,
+}
